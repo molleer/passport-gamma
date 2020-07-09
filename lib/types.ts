e0a4ff3eed73b97e01c254e3623e8c17baa8100e
David Möller <davidm@student.chalmers.se>
2020-07-09 20:42:28 +0200

    Added working gamma passport

--- lib/types.ts
+++
@@ -1,7 +0,0 @@
-export interface StrategyOptions {
-    authorizationURL: string;
-    tokenURL: string;
-    clientID: string;
-    clientSecret: string;
-    callbackURL: string;
-}
--- test/src/gamma-config.js
+++ test/src/gamma-config.js
@@ -1,12 +1,13 @@
-const GammaStrategy = require("./strategies/gamma/strategy");
+const GammaStrategy = require("passport-gamma").Strategy;
+const axios = require("axios");
 
 module.exports = {
     init(passport) {
         var strategy = new GammaStrategy(
             {
-                authorizationURL: "http://localhost/api/oauth/authorize",
-                tokenURL: "http://localhost/api/oauth/token",
-                profileURL: "http://localhost/api/users/me",
+                authorizationURL: "http://localhost:8080/api/oauth/authorize",
+                tokenURL: "http://localhost:8080/api/oauth/token",
+                profileURL: "http://localhost:8080/api/users/me",
                 clientID: "id",
                 clientSecret: "secret",
                 callbackURL: "http://localhost:3001/auth/callback"
@@ -14,17 +15,23 @@ module.exports = {
             (accessToken, profile, cb) => {
                 console.log("Verifying");
                 console.log(profile);
-                cb(null, profile);
+                cb(null, { ...profile, accessToken: accessToken });
             }
         );
 
         passport.use(strategy);
-        passport.serializeUser(function (user, cb) {
-            cb(null, user);
-        });
+        passport.deserializeUser(async (obj, cb) => {
+            console.log("deserializing");
+            const res = await axios.get("http://localhost:8080/api/users/me", {
+                headers: {
+                    Authorization: `Bearer ${obj.accessToken}`
+                }
+            });
 
-        passport.deserializeUser(function (obj, cb) {
-            cb(null, obj);
+            return cb(null, { ...res.data, accessToken: obj.accessToken });
+        });
+        passport.serializeUser(function (user, cb) {
+            cb(null, { accessToken: user.accessToken });
         });
     }
 };
